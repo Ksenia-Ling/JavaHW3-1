bfff49a257cfa0151cace8f628b882e2d3e83eac
Ksenia Kozyreva <oestara91@gmail.com>
2020-05-02 13:04:52 +0300

    Miles 2 added

---
+++ Miles2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int TicketCost = 3518;
+        int CondInRub = 20;
+        int bonus = 1;
+        int Totalbonus = (TicketCost / CondInRub) * bonus;
+
+    System.out.println(Totalbonus);
+}
+}
